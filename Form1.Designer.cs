632fc62808805d3c0be9da02410862955b470643
Administrator <Administrator@LAB1-MAY19>
2025-02-17 11:31:01 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace bai8_pheptinhcoban_radioButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSo1 = new System.Windows.Forms.Label();
+            this.lblSo2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSo1 = new System.Windows.Forms.TextBox();
+            this.txtSo2 = new System.Windows.Forms.TextBox();
+            this.btnTinh = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdbCong = new System.Windows.Forms.RadioButton();
+            this.rdbTru = new System.Windows.Forms.RadioButton();
+            this.rdbNhan = new System.Windows.Forms.RadioButton();
+            this.rdbChia = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(267, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Phép tính cơ bản Button";
+            // 
+            // lblSo1
+            // 
+            this.lblSo1.AutoSize = true;
+            this.lblSo1.Location = new System.Drawing.Point(72, 74);
+            this.lblSo1.Name = "lblSo1";
+            this.lblSo1.Size = new System.Drawing.Size(24, 13);
+            this.lblSo1.TabIndex = 1;
+            this.lblSo1.Text = "so1";
+            // 
+            // lblSo2
+            // 
+            this.lblSo2.AutoSize = true;
+            this.lblSo2.Location = new System.Drawing.Point(72, 112);
+            this.lblSo2.Name = "lblSo2";
+            this.lblSo2.Size = new System.Drawing.Size(24, 13);
+            this.lblSo2.TabIndex = 1;
+            this.lblSo2.Text = "so2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "kết quả";
+            // 
+            // txtSo1
+            // 
+            this.txtSo1.Location = new System.Drawing.Point(170, 74);
+            this.txtSo1.Name = "txtSo1";
+            this.txtSo1.Size = new System.Drawing.Size(100, 20);
+            this.txtSo1.TabIndex = 2;
+            // 
+            // txtSo2
+            // 
+            this.txtSo2.Location = new System.Drawing.Point(163, 109);
+            this.txtSo2.Name = "txtSo2";
+            this.txtSo2.Size = new System.Drawing.Size(100, 20);
+            this.txtSo2.TabIndex = 2;
+            // 
+            // btnTinh
+            // 
+            this.btnTinh.Location = new System.Drawing.Point(170, 209);
+            this.btnTinh.Name = "btnTinh";
+            this.btnTinh.Size = new System.Drawing.Size(75, 23);
+            this.btnTinh.TabIndex = 3;
+            this.btnTinh.Text = "tính";
+            this.btnTinh.UseVisualStyleBackColor = true;
+            this.btnTinh.Click += new System.EventHandler(this.btnTinh_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdbChia);
+            this.groupBox1.Controls.Add(this.rdbNhan);
+            this.groupBox1.Controls.Add(this.rdbTru);
+            this.groupBox1.Controls.Add(this.rdbCong);
+            this.groupBox1.Location = new System.Drawing.Point(315, 71);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(128, 161);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // rdbCong
+            // 
+            this.rdbCong.AutoSize = true;
+            this.rdbCong.Location = new System.Drawing.Point(13, 25);
+            this.rdbCong.Name = "rdbCong";
+            this.rdbCong.Size = new System.Drawing.Size(49, 17);
+            this.rdbCong.TabIndex = 0;
+            this.rdbCong.TabStop = true;
+            this.rdbCong.Text = "cộng";
+            this.rdbCong.UseVisualStyleBackColor = true;
+            // 
+            // rdbTru
+            // 
+            this.rdbTru.AutoSize = true;
+            this.rdbTru.Location = new System.Drawing.Point(19, 59);
+            this.rdbTru.Name = "rdbTru";
+            this.rdbTru.Size = new System.Drawing.Size(37, 17);
+            this.rdbTru.TabIndex = 1;
+            this.rdbTru.TabStop = true;
+            this.rdbTru.Text = "trừ";
+            this.rdbTru.UseVisualStyleBackColor = true;
+            // 
+            // rdbNhan
+            // 
+            this.rdbNhan.AutoSize = true;
+            this.rdbNhan.Location = new System.Drawing.Point(21, 93);
+            this.rdbNhan.Name = "rdbNhan";
+            this.rdbNhan.Size = new System.Drawing.Size(49, 17);
+            this.rdbNhan.TabIndex = 2;
+            this.rdbNhan.TabStop = true;
+            this.rdbNhan.Text = "nhân";
+            this.rdbNhan.UseVisualStyleBackColor = true;
+            // 
+            // rdbChia
+            // 
+            this.rdbChia.AutoSize = true;
+            this.rdbChia.Location = new System.Drawing.Point(18, 125);
+            this.rdbChia.Name = "rdbChia";
+            this.rdbChia.Size = new System.Drawing.Size(45, 17);
+            this.rdbChia.TabIndex = 3;
+            this.rdbChia.TabStop = true;
+            this.rdbChia.Text = "chia";
+            this.rdbChia.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnTinh);
+            this.Controls.Add(this.txtSo2);
+            this.Controls.Add(this.txtSo1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblSo2);
+            this.Controls.Add(this.lblSo1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSo1;
+        private System.Windows.Forms.Label lblSo2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSo1;
+        private System.Windows.Forms.TextBox txtSo2;
+        private System.Windows.Forms.Button btnTinh;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdbChia;
+        private System.Windows.Forms.RadioButton rdbNhan;
+        private System.Windows.Forms.RadioButton rdbTru;
+        private System.Windows.Forms.RadioButton rdbCong;
+    }
+}
+
